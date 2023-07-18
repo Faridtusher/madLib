da07264ec2e2212020153c68e09ff24916a0c196
Farid <mdfaridulislam509@gmail.com>
2023-07-18 16:15:47 +0800

    second commit

---
+++ madlib.py
@@ -0,0 +1,7 @@
+name = input('Please enter your name :')
+father_name = input("Please enter your father name :")
+mother_name =input("Please enter your mother name :")
+brother_name = input("Please enter your brother name :")
+sister_name = input("Please enter your sister name :")
+
+print(f'Hello, {name}! Your father {father_name}, is a good person. Your mother {mother_name} is ia Housewife. Your brother name is {brother_name} and sister is {sister_name}. Alhamdulillah, Happy family' )
